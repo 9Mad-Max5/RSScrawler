a827a389cbaf620c49860d83fdb8f1566be8f4aa
9Mad-Max5 <maximilian.huebner95@gmx.de>
2020-06-14 20:43:46 +0200

    Mergefähig

--- rsscrawler/web.py
+++ rsscrawler/web.py
@@ -36,6 +36,7 @@ from rsscrawler.myjd import package_merge
 from rsscrawler.myjd import remove_from_linkgrabber
 from rsscrawler.myjd import retry_decrypt
 from rsscrawler.myjd import update_jdownloader
+from rsscrawler.notifiers import notify
 from rsscrawler.rsscommon import Unbuffered
 from rsscrawler.rsscommon import decode_base64
 from rsscrawler.rsscommon import get_to_decrypt
@@ -45,7 +46,7 @@ from rsscrawler.rssdb import ListDb
 from rsscrawler.rssdb import RssDb
 
 
-def app_container(port, docker, configfile, dbfile, log_file, no_logger, _device):
+def app_container(port, docker, configfile, dbfile, log_file, no_logger, _device, local_address):
     global device
     device = _device
 
@@ -1018,19 +1019,23 @@ def app_container(port, docker, configfile, dbfile, log_file, no_logger, _device
         else:
             return "Failed", 405
 
-    @app.route(prefix + "/helper/rsscrawler_helper.user.js", methods=['GET'])
+    @app.route(prefix + "/sponsors_helper/rsscrawler_sponsors_helper_sj.user.js", methods=['GET'])
     @requires_auth
-    def rsscrawler_helper():
+    def rsscrawler_sponsors_helper_sj():
         if request.method == 'GET':
             return """// ==UserScript==
-// @name            RSScrawler Helper
+// @name            RSScrawler Sponsors Helper (SJ)
 // @author          rix1337
-// @description     Clicks the correct download button on SJ
-// @version         0.0.3
+// @description     Clicks the correct download button on SJ and forwards decrypted links to RSScrawler
+// @version         0.1.1
 // @require         https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js
 // @match           https://""" + decode_base64("c2VyaWVuanVua2llcy5vcmc=") + """/*
 // @exclude         https://""" + decode_base64("c2VyaWVuanVua2llcy5vcmc=") + """/serie/search?q=*
 // ==/UserScript==
+var sponsorsHelper = false;
+var sponsorsURL = '""" + local_address + """';
+var sponsorsHoster = '';
+
 document.body.addEventListener('mousedown', function(e) {
     if (e.target.tagName != "A") return;
     var anchor = e.target;
@@ -1047,56 +1052,134 @@ var tag = window.location.hash.replace("#", "").split('|');
 var title = tag[0]
 var password = tag[1]
 if (title) {
-    // Check if we need to log in
-    $('.wrapper').prepend('<h3>[RSScrawler Helper] ' + title + '</h3>');
+    // ToDo: Check if we need to log in
+    $('.wrapper').prepend('<h3>[RSScrawler Sponsors Helper] ' + title + '</h3>');
     $(".container").hide();
     var checkExist = setInterval(async function() {
         if ($("tr:contains('" + title + "')").length) {
             $(".container").show();
-            await Sleep(500);
-            if ($("button:contains('User login')").length) {
-                $("button:contains('User login')").click;
-                $("#input-email").val("rix@rix.ninja");
-                $("#input-password").val("qD$8K54Eea58ZTkysggk*cKtC9w3Df");
-                $("button:contains('Login')").click;
-            }
-            await Sleep(500);
             $("tr:contains('" + title + "')")[0].lastChild.firstChild.click();
-            console.log("[RSScrawler Helper] Clicked Download button of " + title);
+            if (sponsorsHelper) {
+                console.log("[RSScrawler Sponsors Helper] Clicked Download button of " + title);
+                await Sleep(500);
+                $("button:contains('filer')").click();
+                $("button:contains('turbo')").click();
+                if (sponsorsHoster) {
+                    $("button:contains('" + sponsorsHoster + "')").click();
+                }
+                console.log("[RSScrawler Sponsors Helper] Clicked Download button to trigger reCAPTCHA");
+            }
+            clearInterval(checkExist);
         }
     }, 100);
 
-    var dlExists = setInterval(async function() {
-        if ($("tr:contains('Download Part')").length) {
-            var items = $("tr:contains('Download Part')").find("a");
-            var links = [];
-            items.each(function(index){
-                links.push(items[index].href);
-            })
-            console.log("[RSScrawler Helper] found download links: " + links);
-            clearInterval(dlExists);
-        }
-    }, 100);
-};""", 200
+    if (sponsorsHelper) {
+        var dlExists = setInterval(function() {
+            if ($("tr:contains('Download Part')").length) {
+                var items = $("tr:contains('Download Part')").find("a");
+                var links = [];
+                items.each(function(index){
+                    links.push(items[index].href);
+                })
+                console.log("[RSScrawler Sponsors Helper] found download links: " + links);
+                clearInterval(dlExists);
+                window.open(sponsorsURL + '/sponsors_helper/to_download/' + btoa(links + '|' + title + '|' + password));
+                // window.close() requires dom.allow_scripts_to_close_windows in Firefox
+                window.close();
+            }
+        }, 100);
+    }
+};
+""", 200
         else:
             return "Failed", 405
 
-    @app.route(prefix + "/helper/to_solve/", methods=['GET'])
+    @app.route(prefix + "/sponsors_helper/rsscrawler_sponsors_helper_fc.user.js", methods=['GET'])
     @requires_auth
-    def to_solve():
+    def rsscrawler_sponsors_helper_fc():
         if request.method == 'GET':
-            to_decrypt = get_to_decrypt(dbfile)
-            if to_decrypt:
-                to_decrypt = to_decrypt[0]
-                name = to_decrypt["name"]
-                url = to_decrypt["url"] + "#" + name + "|" + to_decrypt["password"]
-                return "<a id='link' href=" + url + ">" + name + "</a>", 200
-            else:
-                return "Nothing to decrypt", 200
+            return """// ==UserScript==
+// @name            RSScrawler Sponsors Helper (FC)
+// @author          rix1337
+// @description     Forwards DLC link to RSScrawler
+// @version         0.0.1
+// @match           https://*.""" + decode_base64("ZmlsZWNyeXB0LmNj") + """/*
+// ==/UserScript==
+
+var sponsorsHelper = false;
+var sponsorsURL = '""" + local_address + """';
+
+var tag = window.location.hash.replace("#", "").split('|');
+var title = tag[0]
+var password = tag[1]
+var ids = tag[2]
+
+if (sponsorsHelper) {
+    var dlcExists = setInterval(function() {
+        if (document.getElementsByClassName("dlcdownload").length) {
+            var link = document.getElementsByClassName("dlcdownload")[0].getAttribute('onclick');
+            console.log("[RSScrawler Helper] found download links: " + link);
+            clearInterval(dlcExists);
+            window.open(sponsorsURL + '/sponsors_helper/to_download/' + btoa(link + '|' + title + '|' + password + '|' + ids ));
+            // window.close() requires dom.allow_scripts_to_close_windows in Firefox
+            window.close();
+        }
+    }, 100);
+}
+""", 200
         else:
             return "Failed", 405
 
-    @app.route(prefix + "/helper/to_download/<payload>", methods=['GET'])
+    @app.route(prefix + "/sponsors_helper/", methods=['GET'])
+    @requires_auth
+    def to_decrypt():
+        if request.method == 'GET':
+            return render_template('helper.html')
+        else:
+            return "Failed", 405
+
+    @app.route(prefix + "/sponsors_helper/api/to_decrypt/", methods=['GET'])
+    @requires_auth
+    def to_decrypt_api():
+        if request.method == 'GET':
+            global device
+
+            decrypt_name = False
+            decrypt_url = False
+            decrypt = get_to_decrypt(dbfile)
+            if decrypt:
+                decrypt = decrypt[0]
+                decrypt_name = decrypt["name"]
+                decrypt_url = decrypt["url"] + "#" + decrypt_name + "|" + decrypt["password"]
+
+            failed_name = False
+            failed_url = False
+            failed = get_info(configfile, device)
+            if failed:
+                device = failed[0]
+                failed_packages = failed[4][3]
+                if failed_packages:
+                    failed = failed_packages[0]
+                    failed_name = failed["name"]
+                    failed_url = failed["url"] + "#" + failed_name + "|" + "|" + str(failed["linkids"]) + "," + str(
+                        failed["uuid"])
+
+            return jsonify(
+                {
+                    "to_decrypt": {
+                        "name": decrypt_name,
+                        "url": decrypt_url,
+                    },
+                    "failed": {
+                        "name": failed_name,
+                        "url": failed_url,
+                    }
+                }
+            )
+        else:
+            return "Failed", 405
+
+    @app.route(prefix + "/sponsors_helper/to_download/<payload>", methods=['GET'])
     @requires_auth
     def to_download(payload):
         global device
@@ -1112,10 +1195,46 @@ if (title) {
                     password = payload[2]
                 except:
                     password = ""
+                try:
+                    ids = payload[3]
+                except:
+                    ids = False
+                try:
+                    dlc = re.findall(r"DownloadDLC\(\'(.*)\'\)", links)
+                    if dlc:
+                        links = "https://" + decode_base64("d3d3LmZpbGVjcnlwdC5jYw==") + "/DLC/" + dlc[0] + ".dlc"
+                except:
+                    pass
                 device = download(configfile, dbfile, device, name, "RSScrawler", links, password)
                 if device:
-                    remove_decrypt(name, dbfile)
-                    return "Success", 200
+                    if ids:
+                        ids = ids.split(",")
+                        linkids = ids[0]
+                        uuids = ids[1]
+
+                        linkids_raw = ast.literal_eval(linkids)
+                        linkids = []
+                        if isinstance(linkids_raw, (list, tuple)):
+                            for linkid in linkids_raw:
+                                linkids.append(linkid)
+                        else:
+                            linkids.append(linkids_raw)
+                        uuids_raw = ast.literal_eval(uuids)
+                        uuids = []
+                        if isinstance(uuids_raw, (list, tuple)):
+                            for uuid in uuids_raw:
+                                uuids.append(uuid)
+                        else:
+                            uuids.append(uuids_raw)
+
+                        remove_from_linkgrabber(configfile, device, linkids, uuids)
+                    else:
+                        remove_decrypt(name, dbfile)
+                    notify(["[RSScrawler Sponsors Helper erfolgreich] - " + name], configfile)
+                    print(u"[RSScrawler Sponsors Helper erfolgreich] - " + name)
+                    return "<script type='text/javascript'>" \
+                           "function closeWindow(){window.close()}window.onload=closeWindow;</script>" \
+                           "This requires dom.allow_scripts_to_close_windows in Firefox to close automatically", 200
             return "Failed", 400
         else:
             return "Failed", 405
@@ -1124,7 +1243,7 @@ if (title) {
     http_server.serve_forever()
 
 
-def start(port, docker, configfile, dbfile, log_level, log_file, log_format, _device):
+def start(port, docker, configfile, dbfile, log_level, log_file, log_format, _device, local_address):
     sys.stdout = Unbuffered(sys.stdout)
 
     logger = logging.getLogger('rsscrawler')
@@ -1158,4 +1277,4 @@ def start(port, docker, configfile, dbfile, log_level, log_file, log_format, _de
         print(u'Update steht bereit (' + updateversion +
               ')! Weitere Informationen unter https://github.com/rix1337/RSScrawler/releases/latest')
 
-    app_container(port, docker, configfile, dbfile, log_file, no_logger, _device)
+    app_container(port, docker, configfile, dbfile, log_file, no_logger, _device, local_address)
