78b98d6349bc2680525bfe57c625e6e28e7625ef
9Mad-Max5 <maximilian.huebner95@gmx.de>
2020-10-15 22:23:34 +0200

    new eps

--- rsscrawler/ombi.py
+++ rsscrawler/ombi.py
@@ -229,6 +229,7 @@ def ombi(configfile, dbfile, device, log_debug):
                                     eps.append(enr)
 
                                 elif db.retrieve('show_' + str(imdb_id) + '_' + se) == 'search':
+                                    eps.append(enr)
                                     show_titse = show_tit
                                     show_tits = show_tit
                                     show_titse += se
