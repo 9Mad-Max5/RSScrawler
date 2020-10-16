3d41640faf1aa3fd1d7a9149136f2cade326df05
9Mad-Max5 <maximilian.huebner95@gmx.de>
2020-10-16 14:02:15 +0200

    regex string

--- rsscrawler/ombi.py
+++ rsscrawler/ombi.py
@@ -254,7 +254,7 @@ def ombi(configfile, dbfile, device, log_debug):
                                         show_tit, se)
                                     # download_show(show_tit_search)
                                     payload = search.best_result_sj(
-                                        show_tit, configfile, dbfile)
+                                        show_tits, configfile, dbfile)
                                     if payload:
                                         matches = search.download_sj(
                                             payload, configfile, dbfile)
